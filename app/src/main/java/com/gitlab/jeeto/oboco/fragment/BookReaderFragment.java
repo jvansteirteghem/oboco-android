573f0d39f8c9cb3f953bac0a51e656d6c2ff809f
jeeto jeeto <jeeto.jeeto@gmail.com>
2021-11-19 13:42:45 +0100

    fullscreen

--- app/src/main/java/com/gitlab/jeeto/oboco/fragment/BookReaderFragment.java
+++ app/src/main/java/com/gitlab/jeeto/oboco/fragment/BookReaderFragment.java
@@ -18,6 +18,7 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -29,6 +30,9 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -41,7 +45,6 @@ import com.gitlab.jeeto.oboco.activity.BookReaderActivity;
 import com.gitlab.jeeto.oboco.client.BookDto;
 import com.gitlab.jeeto.oboco.client.BookMarkDto;
 import com.gitlab.jeeto.oboco.common.BaseViewModelProviderFactory;
-import com.gitlab.jeeto.oboco.common.Utils;
 import com.gitlab.jeeto.oboco.view.BookViewPager;
 import com.gitlab.jeeto.oboco.view.PageImageView;
 import com.squareup.picasso.Picasso;
@@ -110,6 +113,28 @@ public class BookReaderFragment extends Fragment implements View.OnTouchListener
         } else if(mMode == BookReaderViewModel.Mode.MODE_LOCAL) {
             mViewModel = new ViewModelProvider(this, new BaseViewModelProviderFactory(getActivity().getApplication(), getArguments())).get(LocalBookReaderViewModel.class);
         }
+    }
+
+    private void hideSystemBars() {
+        Window window = getActivity().getWindow();
+        WindowCompat.setDecorFitsSystemWindows(window, false);
+        WindowInsetsControllerCompat controller = new WindowInsetsControllerCompat(window, window.getDecorView());
+        controller.hide(WindowInsetsCompat.Type.systemBars());
+        controller.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+    }
+
+    private void showSystemBars() {
+        Window window = getActivity().getWindow();
+        WindowCompat.setDecorFitsSystemWindows(window, true);
+        WindowInsetsControllerCompat controller = new WindowInsetsControllerCompat(window, window.getDecorView());
+        controller.show(WindowInsetsCompat.Type.systemBars());
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        mViewModel.setIsFullscreen(false);
     }
 
     @Override
@@ -196,16 +221,6 @@ public class BookReaderFragment extends Fragment implements View.OnTouchListener
                 }
             }
         });
-        mViewPager.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
-            @Override
-            public void onSystemUiVisibilityChange(int visibility) {
-                if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
-                    if(mViewModel.getIsFullscreen()) {
-                        mViewModel.setIsFullscreen(true);
-                    }
-                }
-            }
-        });
 
         mViewModel.getBookObservable().observe(getViewLifecycleOwner(), new Observer<BookDto>() {
             @Override
@@ -234,6 +249,8 @@ public class BookReaderFragment extends Fragment implements View.OnTouchListener
             public void onChanged(Boolean showOpenSelectedBookDialog) {
                 if(showOpenSelectedBookDialog) {
                     if(mOpenBookDialog == null) {
+                        mViewModel.setIsFullscreen(false);
+
                         mOpenBookDialog = new AlertDialog.Builder(getActivity(), R.style.AppCompatAlertDialogStyle)
                                 .setTitle(R.string.book_reader_dialog_open)
                                 .setMessage(mViewModel.getSelectedBook().getName())
@@ -284,30 +301,15 @@ public class BookReaderFragment extends Fragment implements View.OnTouchListener
                         actionBar.hide();
                     }
 
-                    int flag =
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                    | View.SYSTEM_UI_FLAG_FULLSCREEN;
-                    if (Utils.isKitKatOrLater()) {
-                        flag |= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
-                        flag |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-                        flag |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-                    }
-                    mViewPager.setSystemUiVisibility(flag);
-
                     mPageNavLayout.setVisibility(View.INVISIBLE);
+
+                    hideSystemBars();
                 } else {
+                    showSystemBars();
+
                     if (actionBar != null) {
                         actionBar.show();
                     }
-
-                    int flag =
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
-                    if (Utils.isKitKatOrLater()) {
-                        flag |= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
-                    }
-                    mViewPager.setSystemUiVisibility(flag);
 
                     mPageNavLayout.setVisibility(View.VISIBLE);
                 }
